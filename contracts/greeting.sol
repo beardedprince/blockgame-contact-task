18d194e1952aa323c1c31710fcc32db892a20181
"Alaketu Olamilekan Soliu" <"alaketuolamilekan02@gmail.com">
2022-03-10 07:44:51 +0100

    Push files to repo

---
+++ contracts/greeting.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.19;
+
+contract helloWorld  {
+    string private name; // State variable
+
+    constructor() public {
+    name = 'Olamilekan';
+    }
+
+    function userInfo() public {
+
+    }
+
+    function displayUserInfo() public {
+
+    }
+}
